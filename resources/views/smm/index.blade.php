f8442d912d85b5a7b94ad3ccf9b0340ef9c995b0
Fermin <ferminarellano.hn@gmail.com>
2019-01-29 14:09:18 -0600

    Cambios en la plantilla

--- resources/views/smm/index.blade.php
+++ resources/views/smm/index.blade.php
@@ -30,12 +30,8 @@
 									<div class="form-group has-search">
 										<input type="text" class="form-control" placeholder="Buscar">
 									</div>
-									
-									
 								</div>
 							</div>
-							
-							
 						</div>
 					</div>
 				</div>
@@ -55,20 +51,41 @@
 											<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
 											<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
 											<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+											<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+											<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+											<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+											<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
 										  </ol>
 										  <div class="carousel-inner">
 											<div class="carousel-item active">
-											  <img class="d-block w-100" data-src="holder.js/800x350?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_1.jpg') }}" alt="First slide">
 											  <div class="carousel-caption d-none d-md-block">
-												<h5>Buscando el progreso</h5>
-												<p>Mejor que el lorem ipsum</p>
+												<h5>El Sistema de Monitoreo Multiproposito es una iniciativa interinstitucional entre el CREDIA, Gobierno y la Union Europea</h5>
 											  </div>
 											</div>
 											<div class="carousel-item">
-											  <img class="d-block w-100" data-src="holder.js/800x350?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_2.jpg') }}" alt="Second slide">
 											</div>
 											<div class="carousel-item">
-											  <img class="d-block w-100" data-src="holder.js/800x350?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_3.jpg') }}" alt="Third slide">
+											</div>
+											<div class="carousel-item">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_4.jpg') }}" alt="Third slide">
+											</div>
+											<div class="carousel-item">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_5.jpg') }}" alt="Third slide">
+											</div>
+											<div class="carousel-item">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_6.jpg') }}" alt="Third slide">
+											  <div class="carousel-caption d-none d-md-block">
+												<h5>Es un espacio de diálogo</h5>
+											  </div>
+											</div>
+											<div class="carousel-item">
+											  <img class="d-block w-100" src="{{ asset('img/smm-banner/img_7.jpg') }}" alt="Third slide">
+											  <div class="carousel-caption d-none d-md-block">
+												<h5>Para la toma de desiciones.</h5>
+											  </div>
 											</div>
 										  </div>
 										  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
